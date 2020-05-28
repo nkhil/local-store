fa0846cf5a34fc159f3d513f2cf7ec08d631ed64
Nikhil Vijayan <nkhilv@gmail.com>
2020-05-28 21:52:05 +0100

    :fire: delete console statements

--- src/helpers/returnAttributes.js
+++ src/helpers/returnAttributes.js
@@ -1,13 +1,10 @@
 
 export function returnAttributes(attributes) {
-  console.log("returnAttributes -> attributes", attributes)
   const { weight: { weight, unit }, quantity } = attributes;
   if (weight === 0 && !!quantity) {
-    console.log('weight is 0 and quantity exists');
     return `${quantity}`;
   } else if(weight > 0) {
-    console.log('weight is greater than 0');
     return `${weight}${unit}`;
   }
-  // throw new Error(`Error: Weight is ${weight} & quantity is ${quantity}`);
+  throw new Error(`Error: Weight is ${weight} & quantity is ${quantity}`);
 }
